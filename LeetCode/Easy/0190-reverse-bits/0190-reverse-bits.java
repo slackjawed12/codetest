50fe7ca74c23a055910dc8402c1a9f4db62715eb
narustudio <77224652+slackjawed12@users.noreply.github.com>
2023-04-17 11:40:45 +0900

    Time: 1 ms (93.79%), Space: 42.4 MB (16.52%) - LeetHub

--- LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.java
+++ LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.java
@@ -1,14 +1,11 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int answer = 0;
-        int digit = 0;
-        
-        while(digit!=32) {
-            int bit = (n >> (31-digit)) & 1;
-            answer |= bit<<digit++;
-        }
-        
-        return answer;
-    }
+    n = ((n & 0xFFFFFFFF) << 16 | ((n & 0xFFFFFFFF) >>> 16));       
+    n = ((n & 0x00FF00FF) << 8 | (n & 0xFF00FF00) >>> 8);   
+    n = ((n & 0x0F0F0F0F) << 4) | ((n & 0xF0F0F0F0) >>> 4);
+    n = ((n & 0x33333333) << 2) | ((n & 0xCCCCCCCC) >>> 2);   
+    n = ((n & 0x55555555) << 1) | ((n & 0xAAAAAAAA) >>> 1);   
+    return n;
+}
 }

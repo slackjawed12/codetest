468a57101c20234a0e91e47c3faa53728c665815
narustudio <77224652+slackjawed12@users.noreply.github.com>
2023-04-28 11:08:38 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(rd.readLine());
+        System.out.println(N+"");
+        System.out.println("1");
+    }
+}

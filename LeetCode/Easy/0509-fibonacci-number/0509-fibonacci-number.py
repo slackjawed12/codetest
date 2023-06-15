df7dd3a1415b7b6045aeb2ea8ef3b9d01504801c
narustudio <77224652+slackjawed12@users.noreply.github.com>
2023-06-15 09:02:59 +0900

    Time: 49 ms (50.99%), Space: 16.4 MB (14.54%) - LeetHub

---
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = [0, 1]
+        for i in range(2, n+1, 1):
+            dp.append(dp[i-2]+dp[i-1])
+
+        return dp[n];
+            

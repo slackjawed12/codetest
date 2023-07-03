e5c3ec85ca24d00c5b1a24866253c5964262f867
narustudio <77224652+slackjawed12@users.noreply.github.com>
2023-07-03 20:33:35 +0900

    Time: 66 ms (33.48%), Space: 41.9 MB (83.55%) - LeetHub

---
+++ LeetCode/Easy/0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    return stones.split('').reduce((prev, cur)=> prev+=jewels.includes(cur),0);
+};

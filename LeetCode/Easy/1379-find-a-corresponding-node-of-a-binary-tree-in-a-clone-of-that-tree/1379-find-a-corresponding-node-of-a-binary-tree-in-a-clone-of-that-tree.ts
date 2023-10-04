3d9650966416ba3cfcd579686d781b35d26d5eea
minjae lee <77224652+slackjawed12@users.noreply.github.com>
2023-10-04 20:12:23 +0900

    Time: 207 ms (70.83%), Space: 86 MB (44.44%) - LeetHub

---
+++ LeetCode/Easy/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function getTargetCopy(original: TreeNode | null, cloned: TreeNode | null, target: TreeNode | null): TreeNode | null {
+    if(cloned === null) {
+        return null;
+    }
+    
+    if(target.val === cloned.val) {
+        return cloned;
+    };
+    
+    return getTargetCopy(original, cloned.right, target) ?? getTargetCopy(original, cloned.left, target)
+};

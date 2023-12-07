f3f2c7a656e42f14005ae885bd203c54a25ae879
narustudio <slackjawed12@gmail.com>
2023-12-07 22:00:01 +0900

    [SQL][group] latest login in 2020

---
+++ LeetCode/Easy/1890-the-lastest-login-in-2020/1890-the-lastest-login-in-2020.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT 
+    user_id,
+    MAX(time_stamp) as last_stamp
+FROM Logins
+WHERE time_stamp >= '2020-01-01' AND time_stamp < '2021-01-01'
+GROUP BY user_id

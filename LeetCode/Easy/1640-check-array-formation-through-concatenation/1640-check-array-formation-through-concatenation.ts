f8172639f055e98dc19924d41523b090a17f163d
narustudio <slackjawed12@gmail.com>
2024-01-13 16:15:24 +0900

    이전 문제 주석 추가

--- LeetCode/Easy/1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.ts
+++ LeetCode/Easy/1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.ts
@@ -4,9 +4,33 @@ function canFormArray(arr: number[], pieces: number[][]): boolean {
     return prev;
   }, new Map());
 
+  // shift 연산 비효율
+  while (arr.length !== 0) {
+    if (heads.get(arr[0]) === undefined) {
+      return false;
+    }
+
+    const targetPiece = pieces[heads.get(arr[0])];
+    while (targetPiece.length !== 0 && arr[0] === targetPiece[0]) {
+      arr.shift();
+      targetPiece.shift();
+    }
+  }
+
+  return true;
+}
+
+function canFormArrayV2(arr: number[], pieces: number[][]): boolean {
+  const heads = pieces.reduce((prev, cur, index) => {
+    prev.set(cur[0], index);
+    return prev;
+  }, new Map());
+
   let result: number[] = [];
+
   for (const num of arr) {
     const index = heads.get(num);
+    // !index 로 적으면 0일때 true이므로 틀림
     if (index === undefined) {
       continue;
     }

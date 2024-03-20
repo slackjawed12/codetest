2a0c0db75df360a469d99d8dd6a641651202f239
minjae lee <77224652+slackjawed12@users.noreply.github.com>
2024-03-20 23:58:46 +0900

    [level 2] Title: 과제 진행하기, Time: 150.14 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,28 +1,19 @@
 def solution(plans):
-    answer = []
-    converted = [(work[0], to_time(work[1]), int(work[2])) for work in plans]
-    converted.sort(key=lambda x: x[1])
-    stack = []
-    for i in range(len(converted)-1):
-        name, start, work_time = converted[i]
-        stack.append((name,work_time))
-        time_gap = converted[i+1][1] - start
-        while stack and time_gap>0:
-            last = stack.pop()
-            name, left_time = last
-            if left_time <= time_gap:
-                time_gap -= left_time
-                answer.append(last[0])
-            else:
-                stack.append((name, left_time-time_gap))
-                time_gap = 0
-    
-    answer.append(converted[-1][0])
-    while stack:
-        work = stack.pop()
-        answer.append(work[0])
+    plans = sorted(map(lambda x: [x[0], int(x[1][:2]) * 60 + int(x[1][3:]), int(x[2])], plans), key=lambda x: -x[1])
+
+    lst = []
+    while plans:
+        x = plans.pop()
+        for i, v in enumerate(lst):
+            prev_end_time = v[0]
+            end_time = x[1]+x[2]
+            if v[0] > x[1]:
+                lst[i][0] += x[2]
+        lst.append([x[1] + x[2], x[0]])
+    lst.sort()
+
         
-    return answer
+    return [l[1] for l in lst]
 
 def to_time(start:str):
     hour, minute = start.split(":")

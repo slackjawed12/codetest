ffec07654cbc59ee8503b050471161b6e0de2879
narustudio <slackjawed12@gmail.com>
2024-06-04 21:33:28 +0900

    [array] min max game

---
+++ LeetCode/Easy/2293-min-max-game/2293-min-max-game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minMaxGame(self, nums: list[int]) -> int:
+        temp = nums
+        while len(temp) > 1:
+            new = []
+            for i in range(len(temp)//2):
+                if i % 2 ==0:
+                    new.append(min(temp[2*i], temp[2*i+1]))
+                else:
+                    new.append(max(temp[2*i], temp[2*i+1]))
+            
+            temp = new
+        
+        return temp[0]
+
+                

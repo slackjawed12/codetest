95b6fed51f78fa7c4ded845b5cbb857c5b84f57b
narustudio <slackjawed12@gmail.com>
2024-06-25 21:20:14 +0900

    [array] count equal and divisible pairs

---
+++ LeetCode/Easy/2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countPairs(self, nums: list[int], k: int) -> int:
+        result =0
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums[j] and (i*j)%k == 0:
+                    result += 1
+
+        return result
+            

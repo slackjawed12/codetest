93f6eb9239a6b2c586e091a164f24fa7a4478144
narustudio <slackjawed12@gmail.com>
2024-07-02 20:50:36 +0900

    [greedy] make array zero by sub same amount - appendix

--- LeetCode/Easy/2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ LeetCode/Easy/2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -15,6 +15,10 @@ class Solution:
         return self.helper([max(num-x, 0) for num in nums], result+1)
   
 
+# we have to subtract the same amount from all elements in the array
+# Elements with the same value will always take the same number of operations to become 0. 
+# Contrarily, elements with different values will always take a different number of operations to become 0.
+# therefore, operation count is equal to the number of unique non-zero elements in the array
 class Solution2:
     def minimumOperations(self, nums: list[int]) -> int:
         return (len(set(nums)-{0}))

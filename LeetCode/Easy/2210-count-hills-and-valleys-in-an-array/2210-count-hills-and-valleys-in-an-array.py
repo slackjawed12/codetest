fd07187b3f5a955e7053b12a247280dd6d2febe1
narustudio <slackjawed12@gmail.com>
2024-08-26 21:54:44 +0900

    [array] hill and valley - caterpillar version

--- LeetCode/Easy/2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
+++ LeetCode/Easy/2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
@@ -22,3 +22,20 @@ class Solution:
             before = cur
         
         return result
+
+# caterpillar
+class Solution2:
+    def countHillValley(self, nums: list[int]) -> int:
+        result = 0
+        left = nums[0]
+        for i in range(1, len(nums)-1):
+            if nums[i] == nums[i+1]:
+                continue
+
+            # 같은 값이 연속일 때는 가장 오른쪽에 있는 것으로 비교하면 된다.
+            if (nums[i] > left and nums[i] > nums[i+1]) or (nums[i] < left and nums[i] < nums[i+1]):
+                result += 1
+
+            left = nums[i]
+        
+        return result

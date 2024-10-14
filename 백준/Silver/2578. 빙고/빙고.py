7e4548829d668b383d7ba2a4ee8f35f3fccd8f61
minjae lee <77224652+slackjawed12@users.noreply.github.com>
2024-10-14 20:39:31 +0900

    [Silver IV] Title: 빙고, Time: 180 ms, Memory: 119664 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,76 +1,85 @@
+# 빙고
 import sys
-
-board = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(5)]
-call = []
-for _ in range(5):
-    call += list(map(int, sys.stdin.readline().strip().split()))
+from typing import List
 
 
-def is_bingo(info):
-    row = check_row_bingo(info)
-    col = check_col_bingo(info)
-    diag = check_diagonal_bingo(info)
-    if row + col + diag >= 3:
-        return True
+class Solution:
+    def solve(self, board: List[List[int]], call: List[List[int]]) -> int:
+        flat_call = sum(call, [])
+        cur = [[False] * 5 for _ in range(5)]
+        res = 0
+        for i, n in enumerate(flat_call):
+            self.update_board(n, board, cur)
 
-    return False
+            if self.is_bingo(cur):
+                res = i + 1
+                break
 
+        return res
 
-def check_row_bingo(info):
-    result = 0
-    for r in info:
-        if r.count(True) == len(r):
+    def update_board(self, n, board, info: List[List[bool]]):
+        for y, row in enumerate(board):
+            for x, number in enumerate(row):
+                if number == n:
+                    info[y][x] = True
+
+        return
+
+    def is_bingo(self, info: List[List[bool]]) -> bool:
+        row = self.count_row(info)
+        col = self.count_col(info)
+        diag = self.count_diagonal(info)
+        return row + col + diag >= 3
+
+    def count_row(self, info: List[List[bool]]) -> int:
+        result = 0
+        for r in info:
+            if r.count(True) == len(r):
+                result += 1
+
+        return result
+
+    def count_col(self, info: List[List[bool]]) -> int:
+        result = 0
+        for i in range(len(info[0])):
+            temp = 0
+            for j in range(len(info)):
+                if info[j][i]:
+                    temp += 1
+
+            if temp == len(info[0]):
+                result += 1
+
+        return result
+
+    def count_diagonal(self, info: List[List[bool]]) -> int:
+        result = 0
+        dec = 0
+        for i in range(len(info)):
+            if info[i][i]:
+                dec += 1
+
+        if dec == len(info):
             result += 1
 
-    return result
-
-
-def check_col_bingo(info):
-    result = 0
-    for i in range(len(info[0])):
-        temp = 0
+        inc = 0
         for j in range(len(info)):
-            if info[j][i]:
-                temp += 1
+            if info[j][len(info) - j - 1]:
+                inc += 1
 
-        if temp == len(info[0]):
+        if inc == len(info):
             result += 1
 
-    return result
+        return result
 
 
-def check_diagonal_bingo(info):
-    result = 0
-    dec = 0
-    for i in range(len(info)):
-        if info[i][i]:
-            dec += 1
-
-    if dec == len(info):
-        result += 1
-
-    inc = 0
-    for j in range(len(info)):
-        if info[j][len(info) - j - 1]:
-            inc += 1
-
-    if inc == len(info):
-        result += 1
-
-    return result
+def main():
+    board = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(5)]
+    call = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(5)]
+    sol = Solution()
+    answer = sol.solve(board, call)
+    print(answer)
 
 
-cur = [[False] * 5 for _ in range(5)]
-
-answer = 0
-for i, n in enumerate(call):
-    for y, row in enumerate(board):
-        for x, number in enumerate(row):
-            if number == n:
-                cur[y][x] = True
-
-    if is_bingo(cur):
-        answer = i + 1
-        break
-
-print(answer)
+if __name__ == '__main__':
+    main()

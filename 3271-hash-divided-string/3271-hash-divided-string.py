b59416722de05eefca0495e6a234e13469b68e78
minjae lee <77224652+slackjawed12@users.noreply.github.com>
2024-12-29 20:49:21 +0900

    Time: 19 ms (41.74%), Space: 17.9 MB (11.05%) - LeetHub

--- 3271-hash-divided-string/3271-hash-divided-string.py
+++ 3271-hash-divided-string/3271-hash-divided-string.py
@@ -2,13 +2,12 @@ class Solution:
     def stringHash(self, s: str, k: int) -> str:
         result = ""
         
+        cur = 0
         for i in range(len(s)):
+            cur += ord(s[i])-ord('a')
             if (i+1)%k == 0:
-                tmp = 0
-                for j in range(i-k+1, i+1):
-                    tmp += ord(s[j])-ord('a')
-            
-                result += chr(tmp%26+ord('a'))
+                result += chr(cur%26+ord('a'))
+                cur = 0
                     
         return result
         

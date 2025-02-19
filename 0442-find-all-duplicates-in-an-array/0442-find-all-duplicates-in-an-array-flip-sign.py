09ab14b387d1ab1cce781c2be55fd16679eff7e2
minjae lee <77224652+minjaeleedev@users.noreply.github.com>
2025-02-19 20:35:18 +0900

    Time: 36 ms (34.79%), Space: 27.1 MB (31.28%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array-flip-sign.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        result = []
+        n = len(nums)
+        for i in range(n):
+            num = abs(nums[i])
+            idx = num-1
+            if nums[idx] < 0:
+                result.append(num)
+            
+            nums[idx] = -nums[idx]
+        
+        return result
